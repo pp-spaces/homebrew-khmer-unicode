08574dd44aa542224239dd3aeeb1427b539772c1
Socheat Sok <alex@socheat.net>
2020-02-11 11:01:42 +0700

    set `version` to latest and remove check `sha256`

--- Casks/font-open-khmer-school.rb
+++ Casks/font-open-khmer-school.rb
@@ -1,6 +1,6 @@
 cask 'font-open-khmer-school' do
-  version '1.4.2'
-  sha256 'd75aaa2f8588bceaab4ae4ccfcb8cdf9427c144c6eb097d36259591063fe8ba4'
+  version :latest
+  sha256 :no_check
 
   url "https://github.com/OpenInstituteCambodia/open-khmer-school/archive/v1.420.zip"
   name 'Open Khmer School'
