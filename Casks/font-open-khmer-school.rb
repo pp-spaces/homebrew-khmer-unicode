9118d2ad752b9ff2435a8bf8716d9f5e11f9e4c8
Socheat Sok <alex@socheat.net>
2020-02-11 10:39:02 +0700

    add font-open-khmer-school.rb, source: OpenInstituteCambodia@github

---
+++ Casks/font-open-khmer-school.rb
@@ -0,0 +1,14 @@
+cask 'font-open-khmer-school' do
+  version '1.4.2'
+  sha256 'd75aaa2f8588bceaab4ae4ccfcb8cdf9427c144c6eb097d36259591063fe8ba4'
+
+  url "https://github.com/OpenInstituteCambodia/open-khmer-school/archive/v1.420.zip"
+  name 'Open Khmer School'
+  homepage 'https://github.com/OpenInstituteCambodia/open-khmer-school'
+
+  font 'fonts/Normal/OpenKhmerSchool-Bold.ttf'
+  font 'fonts/Normal/OpenKhmerSchool-Light.ttf'
+  font 'fonts/Normal/OpenKhmerSchool-Medium.ttf'
+  font 'fonts/Normal/OpenKhmerSchool-Regular.ttf'
+  font 'fonts/Normal/OpenKhmerSchool-SemiBold.ttf'
+end
