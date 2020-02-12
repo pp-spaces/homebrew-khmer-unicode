5e8c8d34cbe19a07704f2fbda602dfbf03120e41
Socheat Sok <alex@socheat.net>
2020-02-12 09:21:53 +0700

    fix missing KhmerOS.ttf, add `appcast` attribute

--- Casks/font-khmer-os-bundle.rb
+++ Casks/font-khmer-os-bundle.rb
@@ -1,8 +1,9 @@
 cask 'font-khmer-os-bundle' do
-  version :latest
+  version '5.0.0'
   sha256 :no_check
 
-  url "https://github.com/KhmerSoftwareInitiative/khmer-unicode-fonts/releases/download/v5.0.0/khmer-unicode-fonts-v5.0.zip"
+  url "https://github.com/KhmerSoftwareInitiative/khmer-unicode-fonts/releases/download/v#{version}/khmer-unicode-fonts-v#{version}.zip"
+  appcast 'https://github.com/KhmerSoftwareInitiative/khmer-unicode-fonts/releases.atom'
   name 'Open Khmer School'
   homepage 'https://github.com/KhmerSoftwareInitiative'
 
