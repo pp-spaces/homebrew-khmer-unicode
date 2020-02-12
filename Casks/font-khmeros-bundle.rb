d64fd69e3a2802ba534fde5602ae7e5417bfc8b4
Socheat Sok <alex@socheat.net>
2020-02-12 09:28:40 +0700

    change cask name to khmeros-bundle

--- Casks/font-khmeros-bundle.rb
+++ Casks/font-khmeros-bundle.rb
@@ -1,10 +1,10 @@
-cask 'font-khmer-os-bundle' do
+cask 'font-khmeros-bundle' do
   version '5.0.0'
   sha256 :no_check
 
   url "https://github.com/KhmerSoftwareInitiative/khmer-unicode-fonts/releases/download/v#{version}/khmer-unicode-fonts-v#{version}.zip"
   appcast 'https://github.com/KhmerSoftwareInitiative/khmer-unicode-fonts/releases.atom'
-  name 'Open Khmer School'
+  name 'KhmerOS Bundle'
   homepage 'https://github.com/KhmerSoftwareInitiative'
 
   font 'KhmerOS.ttf'
