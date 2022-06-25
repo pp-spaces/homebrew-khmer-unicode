8b4e23c70f6e90a203da326605f00d5566771930
Socheat Sok <alex@socheat.net>
2022-06-25 20:08:00 +0700

    Update khmer-unicode-layout.rb

--- Casks/khmer-unicode-layout.rb
+++ Casks/khmer-unicode-layout.rb
@@ -2,10 +2,10 @@ cask 'khmer-unicode-layout' do
   version '1.1.0'
   sha256 '988cf0e3e8e15efb7ee4c3fad0173566d862f0fd3c88a41a7ae90d25ce819935'
 
-  url "https://github.com/socheatsok78/Khmer-Unicode-for-MacOS/releases/download/v#{version}/Khmer.Unicode.pkg"
-  appcast 'https://github.com/socheatsok78/Khmer-Unicode-for-MacOS/releases.atom'
+  url "https://github.com/socheatsok78/khmer-unicode-keyboard-for-macos/releases/download/v#{version}/Khmer.Unicode.pkg"
+  appcast 'https://github.com/socheatsok78/khmer-unicode-keyboard-for-macos/releases.atom'
   name 'Khmer Unicode Keyboard Layout'
-  homepage 'https://github.com/socheatsok78/Khmer-Unicode-for-MacOS'
+  homepage 'https://github.com/socheatsok78/khmer-unicode-keyboard-for-macos'
 
   pkg "Khmer.Unicode.pkg", allow_untrusted: true
 
