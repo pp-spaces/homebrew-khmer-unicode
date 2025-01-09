4f7f8f9b841c532bcd66823fc713f069d8d6ac41
Socheat Sok <socheatsok78@gmail.com>
2025-01-09 09:49:02 +0700

    Update Casks/font-open-khmer-school.rb

--- Casks/font-open-khmer-school.rb
+++ Casks/font-open-khmer-school.rb
@@ -2,7 +2,7 @@ cask 'font-open-khmer-school' do
   version "1.421"
   sha256 :no_check
 
-  url "https://github.com/OpenInstituteCambodia/open-khmer-school/releases/download/v#{version}/OpenKhmerSchool_v#{version}.zip"
+  url "https://github.com/OpenInstituteCambodia/open-khmer-school/archive/refs/tags/v#{version}.zip"
   name 'Open Khmer School'
   homepage 'https://github.com/OpenInstituteCambodia/open-khmer-school'
 
